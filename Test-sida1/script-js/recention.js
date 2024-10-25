478fe94c1fe9b454c25f29610a42e91cebc6f10d
Banankontrakt <christian1986@live.se>
2024-10-25 19:57:26 +0200

    Delete Test-sida1 directory

--- Test-sida1/script-js/recention.js
+++
@@ -1,7 +0,0 @@
-const stars = document.querySelectorAll('.rating input');
-stars.forEach(star => {
-    star.addEventListener('change', () => {
-        const ratingValue = star.id.replace('star', '');
-        alert(`Du gav ${ratingValue} stjärnor`);
-    });
-});
